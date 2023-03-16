b1a4b9d43399cac22d4717dd87168e2b187e686b
Tom Lonergan <tomlonergan91@gmail.com>
2023-03-16 02:28:24 +0000

    changed to simplelog

--- simulator/src/args.rs
+++ simulator/src/args.rs
@@ -1,20 +1,19 @@
-use log::error;
-
 pub struct Args {
-    pub log_level: log::Level,
+    pub log_level: simplelog::LevelFilter,
     pub path_to_file: String,
+    pub valid: bool,
 }
 
-fn log_level_from_string(log_level: &String) -> log::Level {
-    match log_level.replace("log=", "").as_str() {
-        "trace" => log::Level::Trace,
-        "debug" => log::Level::Debug,
-        "info" => log::Level::Info,
-        "warn" => log::Level::Warn,
-        "error" => log::Level::Error,
+fn log_level_from_string(log_level: &String) -> simplelog::LevelFilter {
+    match log_level.replace("--log=", "").as_str() {
+        "trace" => simplelog::LevelFilter::Trace,
+        "debug" => simplelog::LevelFilter::Debug,
+        "info" => simplelog::LevelFilter::Info,
+        "warn" => simplelog::LevelFilter::Warn,
+        "error" => simplelog::LevelFilter::Error,
         _ => {
             println!("Invalid log level, defaulting to info");
-            return log::Level::Info;
+            return simplelog::LevelFilter::Info;
         }
     }
 }
@@ -22,15 +21,16 @@ fn log_level_from_string(log_level: &String) -> log::Level {
 pub fn parse_args() -> Args {
     let args: Vec<String> = std::env::args().collect();
     let mut parsed_args: Args = Args {
-        log_level: log::Level::Info,
+        log_level: simplelog::LevelFilter::Info,
         path_to_file: String::new(),
+        valid: true,
     };
 
-    let log_level: Vec<&String> = args.iter().filter(|x| x.contains("log=")).collect();
+    let log_level: Vec<&String> = args.iter().filter(|x| x.contains("--log=")).collect();
     match log_level.len() {
         0 => {
-            println!("No log level specified, defaulting to info");
-            parsed_args.log_level = log::Level::Info;
+            println!("No log level specified");
+            parsed_args.log_level = simplelog::LevelFilter::Info;
         }
         1 => parsed_args.log_level = log_level_from_string(log_level[0]),
         _ => {
@@ -38,29 +38,37 @@ pub fn parse_args() -> Args {
             parsed_args.log_level = log_level_from_string(log_level[0])
         }
     }
+    println!("Log level: {}", parsed_args.log_level);
 
-    let path_to_file: Vec<&String> = args.iter().filter(|x| x.contains("file=")).collect();
+    let path_to_file: Vec<String> = args
+        .iter()
+        .filter(|x| x.contains("--file="))
+        .map(|x| x.replace("--file=", ""))
+        .collect();
     match path_to_file.len() {
         0 => {
-            println!("No file specified, defaulting to example_bytecode/basic_addition.ayu");
             if !std::path::Path::new("../example_bytecode/basic_addition.ayu").exists() {
-                error!("Default file not found at '../example_bytecode/basic_addition.ayu'");
+                eprintln!("Default file not found at '../example_bytecode/basic_addition.ayu'");
+                parsed_args.valid = false;
             };
             parsed_args.path_to_file = "../example_bytecode/basic_addition.ayu".to_string();
         }
         1 => {
-            if !std::path::Path::new(path_to_file[0]).exists() {
-                error!("No file at '{}'", path_to_file[0]);
+            if !std::path::Path::new(&path_to_file[0]).exists() {
+                eprintln!("No file at '{}'", path_to_file[0]);
+                parsed_args.valid = false;
             };
-            parsed_args.path_to_file = path_to_file[0].clone()
+            parsed_args.path_to_file = path_to_file[0].clone();
         }
         _ => {
             println!("Multiple files specified, using first option");
-            if !std::path::Path::new(path_to_file[0]).exists() {
-                error!("No file at '{}'", path_to_file[0]);
+            if !std::path::Path::new(&path_to_file[0]).exists() {
+                eprintln!("No file at '{}'", path_to_file[0]);
+                parsed_args.valid = false;
             };
             parsed_args.path_to_file = path_to_file[0].clone();
         }
     }
+    println!("File: {}", parsed_args.path_to_file);
     return parsed_args;
 }
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -6,7 +6,25 @@ mod types;
 
 fn main() {
     let args = args::parse_args();
-    simple_logger::init_with_level(args.log_level).unwrap();
+    if !args.valid {
+        println!("Invalid arguments, terminating");
+        return;
+    }
+    println!("------------------------------------------------------------------------");
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            args.log_level,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Trace,
+            simplelog::Config::default(),
+            File::create("processor.log").unwrap(),
+        ),
+    ])
+    .unwrap();
     // let path_to_file: &String = &args[1];
     let mut processor: processor::Processor = processor::Processor::new(args.path_to_file);
     let mut running: bool = true;
