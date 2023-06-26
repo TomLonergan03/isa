d8c572fa9021f4d7cb9cda3cc7b5eb447048f34c
Tom Lonergan <tomlonergan91@gmail.com>
2023-06-26 23:22:07 +0100

    remove Wrapping type for wrapping_add

--- simulator/src/alu.rs
+++ simulator/src/alu.rs
@@ -18,11 +18,9 @@ impl Alu {
         source_b: u16,
         operation: &AluOperation,
     ) -> AluOutput {
-        let source_a: std::num::Wrapping<u16> = std::num::Wrapping(source_a);
-        let source_b: std::num::Wrapping<u16> = std::num::Wrapping(source_b);
         match operation {
             AluOperation::Add => {
-                let result: u32 = (source_a + source_b).0 as u32;
+                let result: u32 = (source_a.wrapping_add(source_b)) as u32;
                 let zero: bool = result == 0;
                 let negative: bool = match result & 0b1000000000000000 {
                     1 => true,
@@ -35,7 +33,8 @@ impl Alu {
                 };
             }
             AluOperation::Subtract => {
-                let result: u32 = (source_a - source_b).0 as u32;
+                let source_b: u16 = !source_b + 1;
+                let result: u32 = (source_a.wrapping_add(source_b)) as u32;
                 let zero: bool = result == 0;
                 let negative: bool = match result & 0b1000000000000000 {
                     0b1000000000000000 => true,
@@ -48,7 +47,7 @@ impl Alu {
                 };
             }
             AluOperation::And => {
-                let result: u32 = (source_a & source_b).0 as u32;
+                let result: u32 = (source_a & source_b) as u32;
                 let zero: bool = result == 0;
                 let negative: bool = false;
                 return AluOutput {
@@ -58,7 +57,7 @@ impl Alu {
                 };
             }
             AluOperation::Or => {
-                let result: u32 = (source_a | source_b).0 as u32;
+                let result: u32 = (source_a | source_b) as u32;
                 let zero: bool = result == 0;
                 let negative: bool = false;
                 return AluOutput {
@@ -68,8 +67,6 @@ impl Alu {
                 };
             }
             AluOperation::ShiftLeft => {
-                let source_a: u32 = source_a.0 as u32;
-                let source_b: u32 = source_b.0 as u32;
                 let result: u32 = (source_a << source_b) as u32;
                 let zero: bool = result == 0;
                 let negative: bool = false;
@@ -80,8 +77,6 @@ impl Alu {
                 };
             }
             AluOperation::ShiftRightLogical => {
-                let source_a: u32 = source_a.0 as u32;
-                let source_b: u32 = source_b.0 as u32;
                 let result: u32 = (source_a >> source_b) as u32;
                 let zero: bool = result == 0;
                 let negative: bool = false;
@@ -92,8 +87,6 @@ impl Alu {
                 };
             }
             AluOperation::ShiftRightArithmetic => {
-                let source_a: u32 = source_a.0 as u32;
-                let source_b: u32 = source_b.0 as u32;
                 let result: u32 = if (source_a & 0b1000000000000000) == 0b1000000000000000 {
                     // Fill right shift 0 bits with 1s
                     ((source_a) >> source_b) as u32 | (0xFFFF << (16 - source_b))
